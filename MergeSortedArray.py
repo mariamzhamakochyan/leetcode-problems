c367875a42336e9357b5543c92692a7061fb3ae1
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2022-08-20 00:24:54 +0400

    Update MergeSortedArray.py

--- MergeSortedArray.py
+++ MergeSortedArray.py
@@ -1,17 +1,20 @@
-def mergeSort(num1, m, num2, n):
-    while m >= 0 and n >= 0:
-        if num1[m -1] >= num2[n - 1]:
-            num1[m + n - 1] = num1[m - 1]
-            m -= 1
+def arr(num1, m, num2, n):
+    while n > 0 and m > 0:
+        l = m+n-1
+        if num1[m -1] > num2[n-1]:
+            num1[l] = num1[m-1]
+            m-=1
         else:
-            num1[m + n - 1] = num2[n - 1]
-            n -= 1
-    for i in range(0, n):
-        num1[i] = num2[i]
+            num1[l] = num2[n-1]
+            n -=1
+            l -=1
+    while n > 0:
+        num1[l] = num2[n-1]
+        l -= 1
+        n -=1
     return num1
-    
 num1 = [1,2,3,0,0,0]
-num2 = [2,5,6]
 m = 3
+num2 = [2,5,6]
 n = 3
-print(mergeSort(num1, m, num2, n))
+print(arr(num1, m, num2, n))
